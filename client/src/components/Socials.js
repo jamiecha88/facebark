46368b795adfe4861d12f10d1879648744c4ed8a
Jamie Cha <chaeunhee98@gmail.com>
2023-04-05 16:19:11 -0500

    using tss-react/mui instead of makeStyles

--- client/src/components/Socials.js
+++ client/src/components/Socials.js
@@ -1,31 +1,32 @@
-import { makeStyles } from "@material-ui/core/styles";
-import { Grid } from "@material-ui/core";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import HomeIcon from "@material-ui/icons/Home";
-import { socialMedia } from "data/socialMedia";
+import { styled } from "tss-react/mui";
+import { Grid } from "@mui/material";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import HomeIcon from '@mui/icons-material/Home';
 
-const useStyles = makeStyles((theme) => ({
-  snsIcon: {
-    width: "30px",
-    height: "30px",
+const socialMedia = {
+  twitter: "https://twitter.com/jamiecha88",
+  github: "https://github.com/jamiecha88",
+  homepage: "https://jamiecha.netlify.app/",
+};
 
-    [theme.breakpoints.down("xs")]: {
-      width: "25px",
-      height: "25px",
-    },
-    "&:hover": {
-      color: theme.palette.info.main,
-    },
+const SnsIcon = styled("div")(({ theme }) => ({
+  width: "30px",
+  height: "30px",
+
+  [theme.breakpoints.down("xs")]: {
+    width: "25px",
+    height: "25px",
+  },
+  "&:hover": {
+    color: theme.palette.info.main,
   },
 }));
 
-const Social = ({ color }) => {
-  const classes = useStyles();
-  const { instagram, facebook, github, homepage } = socialMedia;
+export default function Social({ color }) {
+  const { github, twitter, homepage } = socialMedia;
   return (
-    <Grid item container spacing={2} justify="center">
+    <Grid item container spacing={2} justifyContent="center">
       <Grid
         item
         component={"a"}
@@ -33,34 +34,12 @@ const Social = ({ color }) => {
         rel="noreferrer noopener"
         href={homepage}
       >
-        <HomeIcon
-          className={classes.snsIcon}
+        <SnsIcon
           color={color ? "primary" : "secondary"}
-        />
-      </Grid>
-      <Grid
-        item
-        component={"a"}
-        target="_blank"
-        rel="noreferrer noopener"
-        href={facebook}
-      >
-        <FacebookIcon
-          className={classes.snsIcon}
-          color={color ? "primary" : "secondary"}
-        />
-      </Grid>
-      <Grid
-        item
-        component={"a"}
-        target="_blank"
-        rel="noreferrer noopener"
-        href={instagram}
-      >
-        <InstagramIcon
-          className={classes.snsIcon}
-          color={color ? "primary" : "secondary"}
-        />
+          sx={{ color: "inherit" }}
+        >
+          <HomeIcon />
+        </SnsIcon>
       </Grid>
       <Grid
         item
@@ -69,13 +48,24 @@ const Social = ({ color }) => {
         rel="noreferrer noopener"
         href={github}
       >
-        <GitHubIcon
-          className={classes.snsIcon}
+        <SnsIcon
           color={color ? "primary" : "secondary"}
-        />
+          sx={{ color: "inherit" }}
+        >
+          <GitHubIcon />
+        </SnsIcon>
+      </Grid>
+      <Grid
+        item
+        component={"a"}
+        target="_blank"
+        rel="noreferrer noopener"
+        href={twitter}
+      >
+        <SnsIcon>
+          <TwitterIcon />
+        </SnsIcon>
       </Grid>
     </Grid>
   );
-};
-
-export default Social;
+}
